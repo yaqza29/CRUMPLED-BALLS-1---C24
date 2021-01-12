15588279c28453ce47731933400f5c871cb42fb3
yaqza29 <75371978+yaqza29@users.noreply.github.com>
2021-01-12 23:15:13 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,34 @@
+class dustbin{
+
+    constructor(x,y,width,height){
+        var options = {
+         isStatic: true
+         ,'restitution':1
+        };
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
+
+    }
+
+
+    display(){
+        var pos = this.body.position;
+        //var angle = this.body.angle;
+
+        this.body.shapeColor = rgb(233, 22, 64);
+        
+        push();
+
+        translate(pos.x, pos.y);
+        fill("red");
+        rectMode(CENTER);
+        rect(pos.x, pos.y, this.width, this.height);
+
+
+        pop();
+ 
+        
+        }
+}
